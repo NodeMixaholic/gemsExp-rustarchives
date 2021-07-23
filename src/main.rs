84fd8f651df6f0104a76ebd6110c6a07557820cc
Samuel Lord <sam@sllord.info>
2021-07-23 18:30:21 +0000

    RuShell Test 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+//Basically just defining "gemsStdio.getCLIInput". Use "println!" for printing to CLI.
+mod gemsStdio;
+mod gemsCalc;
+
 fn main() {
+    let gemsVer = "0.000000001a";
     let mut testvar = 0;
     testvar = 100;
     println!("Welcome to gemsExp RuShell test!
@@ -6,10 +11,22 @@ fn main() {
     from the ground up, but this time in RUST!
     ");
     while true {
-        let mut cmd = String::new();
         println!("RUSHELL>");
-        let b1 = std::io::stdin().read_line(&mut cmd).unwrap();
-        println!("{}", cmd);
+        let cmd = gemsStdio::getCLIInput();
+        let mut argz = cmd.split(" ");
+        let args: Vec<&str> = argz.collect();
+        if (args[0] == "calc") {
+            gemsCalc::calc();
+        } else if (args[0] == "echo") {
+            let echoStr = cmd.replace(args[0], "");
+        } else if (args[0] == "help") {
+            println!("echo - echos stuff
+            calc - a simple calculator");
+        } else if (args[0] == "about") {
+            println!("GEMS Exp v{}", gemsVer)
+        } else {
+            println!("Command not found! (Perhaps try \"help\" or \"about\"?)");
+        }
     }
     println!("PANIC:
     BROKE OUTSIDE OF RUSHELL!")
