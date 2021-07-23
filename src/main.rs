074f87107832f691a56d14edf9d6dfaa61baf3c4
Samuel Lord <sam@sllord.info>
2021-07-23 16:56:15 +0000

    Rushell Test 1

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 fn main() {
     let mut testvar = 0;
     testvar = 100;
-    println!("Welcome to GEMS Experience!
+    println!("Welcome to gemsExp RuShell test!
     This is a brand new rewrite of GEMS NT
     from the ground up, but this time in RUST!
-    This should display the number \"100\": 
     ");
-    println!("{}", testvar);
+    while true {
+        let mut cmd = String::new();
+        println!("RUSHELL>");
+        let b1 = std::io::stdin().read_line(&mut cmd).unwrap();
+        println!("{}", cmd);
+    }
+    println!("PANIC:
+    BROKE OUTSIDE OF RUSHELL!")
 }
